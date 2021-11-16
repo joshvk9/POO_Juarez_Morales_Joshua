f37e837adf9152d4551ea0d0a8071a51a4a29dce
joshvk9 <j.juarez.m3@edu.utc.mx>
2021-11-16 01:22:10 -0600

    Actividades git commit -m Actividades git commit -m Actividades

---
+++ 01_Hola_Mundo/HolaMundo.java
@@ -0,0 +1,33 @@
+//Esto es un comentario
+
+/*
+esto es otro comentario los comentario no van a afectar 
+al codigo fuente de la clase 
+*/ 
+
+class HolaMundo{
+//Cuerpo del programa 
+
+/*
+Aqui es donded nosotros empezamos a codifica todas las funciones 
+que querramos para que el programa 
+su estructura es:
+variable [globales, independiente o solo de la clase]
+
+metodos [acciones a programar]
+*/
+
+//toda clase debe de tener si solo si es 
+//la Clase principal un metodo principla
+
+//el metodo principal es donde se hace la llamada
+//a la acciones de todos el programa 
+
+      public static void main(String[] args){
+      //esto es el cuerpo del metodo principal
+      System.out.print("Hola Mundo.. :3");
+      }
+
+}
+
+
