0c84368d0ed9a8167070e1f87d9bda253701ec66
joshvk9 <j.juarez.m3@edu.utc.mx>
2021-11-19 03:12:04 -0600

    Actividades

---
+++ 06_Figuras/Principal.java
@@ -0,0 +1,13 @@
+/*
+Ejemplo de una clase de figuras
+*/
+
+public class Principal {
+    ublic static void main(String[] args){
+        //Metodo principal 
+        //instancia de la clase 
+        Figura objfigura = new Figura();
+        objfigura.menu();
+    }
+    
+}
