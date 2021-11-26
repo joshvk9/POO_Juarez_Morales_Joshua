ffa6a618bb6b1aad7be57dfa43295577852995f5
joshvk9 <j.juarez.m3@edu.utc.mx>
2021-11-26 07:42:17 -0600

    Actividades

--- 09_Herencia/Perro.java
+++ 09_Herencia/Perro.java
@@ -1,47 +1,49 @@
-
-/*
-Para poder aplicar la herencia de una clase, se utiliza la palabra reservada
-extends 
-esos significa qie ña cñase va a heredar de una clase padre
-
-animal es la clase padre 
-perro es la clase hija
-*/
-
-public class Perro extends animal {
+public class Perro extends Animal {
     /*
-    Aqui deberia de colacar cada atributo 
-    nombre, raza, edad, tipo_alimento, sexo 
+    Para poder aplicar la herencia de una clase, se utiliza la palabra reservada
+    extends
+    Eso significa que la clase va a heredar de una clase padre
+    Animal es la clase padre
+    Perro es la clase hija
+    */ 
+    /*
+    Aqui deberia de colocar cada atributo
+    nombre, raza, edad, tipo_alimento, sexo
     */
 
-    String ladString;
+    String ladrido;
 
-    Public Perro(){
-        //constructor ded la clase
+    public Perro(){
+        //constructor de la clase
+    }
+
+    //sobrecarga para obtener y enviar el ladrido
+    public Perro(String nombre, String raza, String tipo_alimento, 
+    int edad, String ladrido){
+        //para poder obtener los atributos por herencia
+        //se debe de colocar super
+        super(nombre, raza, tipo_alimento, edad);
+        this.ladrido = ladrido;
 
     }
-    //sobrecarga para obtener y enviar el ladrido 
-    public Perro(String nombre, String raza, string tipo_alimento,
-    int edad, boolean sexo, String ladrido){
-        //para poder obtener los atributos por herencia 
-        //se debe de colocar super 
-        super(nombre, raza, tipo_alimento, edad, sexo);
-        this.ladrido = ladrido; 
+    public String getLadrido(){
+        return ladrido;
     }
-    public String getladrido(){
-        retun ladrido;    
-    }
+
     //enviar
-    public voud  setLadrido(){
+    public void setLadrido(String ladrido){
         this.ladrido = ladrido;
     }
-    
-    //metodo para mostrar la informacion del perro 
+
+    //metodo para mostrar la informacion del perro
     public void mostrarPerro(){
-        System.out.println("El nombre del perro es: " + getNombre() +"\n" + "Su raza es: " + getraza() +"\" + "se alimenta de " + ge)
+        System.out.println("El nombre del perro es: " + getNombre() +"\n"
+                            + "Su raza es: " + getRaza() +"\n"
+                            + "Se alimenta de: " + getTipo_alimento() +"\n"
+                            + "Tiene la edad de: " + getEdad() +"\n"
+                            //+ "Su sexo es: " + getSexo() +"\n"
+                            + "Su ladrido es: " + ladrido  +"\n");
     }
-
-
-
-
 }
+    
+
